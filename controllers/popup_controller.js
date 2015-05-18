57f1e02dba8767904ecf24478adc79c14a3f41a6
Rasmus Berggrén <rajje@mac.se>
2015-05-18 10:46:56 +0200

    Small fixes on popup controller

--- controllers/popup_controller.js
+++ controllers/popup_controller.js
@@ -1,43 +1,41 @@
 PopUpController = function(model, mainController, view) {
+	_this = this;
 
 	this.update = function(msg) {
-		if (msg == "loadPopup"){
-			createPopUp("other");
+		if (msg == "loadPopup") {
+			this.createPopUp("other");
 		} 
 	}
 
-		$("#messageGrid").on('click', '.userInfo', function(event){
-			var clickedUser = $(event.target).html();
-			var alias = model.getAlias();
-		
-		if(clickedUser == alias){
-			createPopUp("user");
-		}
-		
-		else{
+	$("#messageGrid").on('click', '.userInfo', function(event){
+		var clickedUser = $(event.target).html();
+		var alias = model.getAlias();
+	
+		if (clickedUser == alias) {
+			_this.createPopUp("user");
+		} else {
 			model.getNewUser(clickedUser); 
 		}
-		
-		});
 	
-		model.subscribe(this);
+	});
+
+	model.subscribe(this);
 		
-		function createPopUp(type) {
-			var person;
-			
-			if(type == "user"){
-				person = model.getUser();
-			}
-			else if(type == "other"){
-				person = model.getOther();
-			}
-			
-			var profilePic = person.profileImage;
-			var name = person.name;
-			var alias = person.alias;
-			$("#userName").empty();
-			$("#userName").append("<p>"+alias+"</p>");
-			$("#userName").prepend("<br><b><p>"+name+"</p></b>");
-			$("#userName").prepend("<img src='"+profilePic+"'>");
+	this.createPopUp = function(type) {
+		var person;
+		
+		if (type == "user") {
+			person = model.getUser();
+		} else if (type == "other") {
+			person = model.getOther();
 		}
+		
+		var profilePic = person.profileImage;
+		var name = person.name;
+		var alias = person.alias;
+		$("#userName").empty();
+		$("#userName").append("<p>"+alias+"</p>");
+		$("#userName").prepend("<br><b><p>"+name+"</p></b>");
+		$("#userName").prepend("<img src='"+profilePic+"'>");
+	}
 }
